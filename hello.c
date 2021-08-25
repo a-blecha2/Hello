06bea842984558ecd16046177b433dfb255344d8
Ava Blecha <ablecha2@huskers.unl.edu>
2021-08-25 23:28:11 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Ava Blecha
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Ava Blecha!\n");
+    printf("Computer Science\n");
+
+    return 0;
+}
+
